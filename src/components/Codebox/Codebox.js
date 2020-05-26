ff3ad72eca404848f33b247e503f0432267ea3e1
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-05-26 22:29:46 +0530

    Added codemirror

--- src/components/Codebox/Codebox.js
+++ src/components/Codebox/Codebox.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { UnControlled as CodeMirror } from "react-codemirror2";
+
 import "./Codebox.scss";
 
+// The following two imports is for the theme.
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+
+// This import is for the language syntax highlighting.
+import "codemirror/mode/xml/xml";
+
 export default function Codebox() {
+  const [mode, setMode] = useState("xml");
+
   return (
     <div className="codebox-container">
       <header>
         <h1>CodeRigade</h1>
       </header>
       <main>
-        <div className="controls"></div>
-        <textarea cols="150" rows="80"></textarea>
+        <div className="controls">Controls go here</div>
+        <CodeMirror
+          value="<h1>I ♥ react-codemirror2</h1>"
+          className="code-editor"
+          options={{
+            mode: "xml",
+            theme: "material",
+            lineNumbers: true,
+          }}
+          onChange={(editor, data, value) => {}}
+        />
+        {/* <textarea cols="150" rows="80"></textarea> */}
       </main>
     </div>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Login.scss";
 
-export default class Login extends Component {
-  render() {
-    return (
-      <div className="login-container">
-        <form>
-          <input type="text" placeholder="Username" required />
-          <input type="text" placeholder="CodeBox key" required />
-          <input type="submit" />
-        </form>
-      </div>
-    );
-  }
+export default function Login() {
+  return (
+    <div className="login-container">
+      <form>
+        <input type="text" placeholder="Username" required />
+        <input type="text" placeholder="CodeBox key" required />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
