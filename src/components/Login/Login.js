b3dfb610349d21fce233925356c2938a037329e8
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-05-26 17:40:57 +0530

    Created login with styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,12 +4,13 @@ import "./login.scss";
 export default class Login extends Component {
   render() {
     return (
-        <div className="login-container">
-          <form>
-            <input type="text" placeholder="Username" />
-            <input type="text" placeholder="Room code" />
-          </form>
-        </div>
+      <div className="login-container">
+        <form>
+          <input type="text" placeholder="Username" required />
+          <input type="text" placeholder="Room code" required />
+          <input type="submit" />
+        </form>
+      </div>
     );
   }
 }
