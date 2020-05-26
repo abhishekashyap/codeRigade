08d7018c08d95450fb98b06a37965a5850ec174a
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-05-27 02:03:49 +0530

    Created control bar with styling

--- src/components/Codebox/Codebox.js
+++ src/components/Codebox/Codebox.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { UnControlled as CodeMirror } from "react-codemirror2";
-
-import "./Codebox.scss";
+import { FiShare2 } from "react-icons/fi";
 
 // The following two imports is for the theme.
 import "codemirror/lib/codemirror.css";
@@ -10,8 +9,15 @@ import "codemirror/theme/material.css";
 // This import is for the language syntax highlighting.
 import "codemirror/mode/xml/xml";
 
+// Overrides some codemirror classes, don't change order
+import "./Codebox.scss";
+
 export default function Codebox() {
-  const [mode, setMode] = useState("xml");
+  const [mode, setMode] = useState({
+    mode: { name: "xml" },
+    theme: "material",
+    lineNumbers: true,
+  });
 
   return (
     <div className="codebox-container">
@@ -19,16 +25,37 @@ export default function Codebox() {
         <h1>CodeRigade</h1>
       </header>
       <main>
-        <div className="controls">Controls go here</div>
+        <div className="controls">
+          <div className="control-dropdown">
+            <select>
+              <option selected value="xml">
+                XML/HTML
+              </option>
+              <option value="css">CSS</option>
+              <option value="js">Javascript</option>
+              <option value="java">Java</option>
+            </select>
+          </div>
+          <div className="control-icon">
+            <span>Share&nbsp;</span>
+            <FiShare2 size={15} />
+          </div>
+          <div className="control-dropdown">
+            <select id="text">
+              <option value="Material">Material</option>
+              <option value="monokai">Monokai</option>
+            </select>
+          </div>
+        </div>
         <CodeMirror
           value="<h1>I ♥ react-codemirror2</h1>"
           className="code-editor"
-          options={{
-            mode: "xml",
-            theme: "material",
-            lineNumbers: true,
+          options={mode}
+          onChange={(editor, data, value) => {
+            console.log("Editor: ", editor);
+            console.log("Data: ", data);
+            console.log("Value: ", value);
           }}
-          onChange={(editor, data, value) => {}}
         />
         {/* <textarea cols="150" rows="80"></textarea> */}
       </main>
